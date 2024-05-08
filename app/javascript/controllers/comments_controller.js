eba68cd694f41a2888be5d3fe2d7f6d5f0756ea9
Niklas_Peper <niklas.peper@kvhh.de>
2024-05-08 16:29:53 +0200

    fixed stimulus controller for comment section toggling

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -1,16 +1,8 @@
-import { Controller } from "stimulus";
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-    static targets = ["collapse", "button"];
-
-    connect() {
-        this.toggleComments();
-    }
-
-    toggleComments() {
-        if (this.hasCollapseTarget) {
-            this.collapseTarget.classList.toggle("show");
-            this.buttonTarget.innerText = this.collapseTarget.classList.contains("show") ? "Hide comments" : "Show comments";
-        }
+    static targets = ["button"];
+    toggleButtonText() {
+        this.buttonTarget.innerText = this.buttonTarget.innerText == "Show Comments" ? "Hide Comments" : "Show Comments";
     }
 }
