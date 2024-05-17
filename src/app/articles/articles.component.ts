93ca70019d5e254b025d1055ae97aa823ff7cf36
Niklas_Peper <niklas.peper@kvhh.de>
2024-05-17 11:02:29 +0200

    implemented @input and @output parameters for parent-child communication

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,21 +1,21 @@
 import { Component } from '@angular/core';
 import { IArticle } from './article.model';
 import { CommonModule } from '@angular/common';
+import { ArticleDetailsComponent } from "../article-details/article-details.component";
 
 @Component({
   selector: 'blog-articles',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './articles.component.html',
   styleUrl: './articles.component.css',
-  // 'styles' array permits direct css: 
-  // styles: ['a { font-weight:bold}']
+  imports: [CommonModule, ArticleDetailsComponent]
 })
 export class ArticlesComponent {
   article: IArticle;
   articles: any;
   filter: string = '';
-  price: number = 420;
+  favouredArticles: IArticle[] = [];
+
   constructor() {
     this.article = {
       title: 'First Test Article',
@@ -49,29 +49,16 @@ export class ArticlesComponent {
       ]
 
   }
-
   createAltTag(article: IArticle) {
     return article.title + ' example pic'
   }
-
   getFilteredArticles() {
     return this.filter === ''
       ? this.articles
       : this.articles.filter((article: any) => article.status === this.filter);
   }
-  // function for ngClass binding
-  // can return a string with the name of the class like 'public'
-  // can return a string with the names of the desired classes like 'public bold'
-  // can return an array with the names of the classes like ['public', 'bold']
-  // --> this allows to push or remove elements to an existing class array
-  getStatusClass(article: IArticle) {
-    return article.status === 'public'
-      ? 'public bold'
-      : article.status === 'private'
-        ? ['private', 'italic']
-        : 'archived'
-    // another possibility to return a css class name:
-    // return { public: article.status === 'public' }
+  addFavouredArticle(article: IArticle) {
+    this.favouredArticles.push(article);
   }
 }
 
