2f81dcdfff191e33c6e5574d3143e6ff8cd441b6
Niklas_Peper <niklas.peper@kvhh.de>
2024-05-17 10:25:31 +0200

    added ngclass directive and styling examples

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -7,10 +7,11 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './articles.component.html',
-  styleUrl: './articles.component.css'
+  styleUrl: './articles.component.css',
+  // 'styles' array permits direct css: 
+  // styles: ['a { font-weight:bold}']
 })
 export class ArticlesComponent {
-
   article: IArticle;
   articles: any;
   filter: string = '';
@@ -58,4 +59,19 @@ export class ArticlesComponent {
       ? this.articles
       : this.articles.filter((article: any) => article.status === this.filter);
   }
+  // function for ngClass binding
+  // can return a string with the name of the class like 'public'
+  // can return a string with the names of the desired classes like 'public bold'
+  // can return an array with the names of the classes like ['public', 'bold']
+  // --> this allows to push or remove elements to an existing class array
+  getStatusClass(article: IArticle) {
+    return article.status === 'public'
+      ? 'public bold'
+      : article.status === 'private'
+        ? ['private', 'italic']
+        : 'archived'
+    // another possibility to return a css class name:
+    // return { public: article.status === 'public' }
+  }
 }
+
