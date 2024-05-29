41efc100b1f578f344f72885ee0113deeecf4449
Niklas_Peper <niklas.peper@kvhh.de>
2024-05-29 16:16:13 +0200

    added error message sign in form

--- ng-frontend/src/app/user/sign-in/sign-in.component.ts
+++ ng-frontend/src/app/user/sign-in/sign-in.component.ts
@@ -3,11 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { IUserCredentials } from '../user.model';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'blog-sign-in',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
@@ -16,18 +17,20 @@ export class SignInComponent {
   errors: string = "";
   private userSvc: UserService = inject(UserService);
   private router: Router = inject(Router);
+  signInError: boolean = false;
 
   constructor() {
 
   }
 
   signIn() {
+    this.signInError = false;
     // calling the user service where the credentials are posted to api
     // subscribers are functions that receive emitted values from an observable. they can handle the data, perform side effects, or unsubscribe from the observable.
     this.userSvc.signIn(this.credentials).subscribe({
       // when the user successfully signed in, navigate to articles page
       next: () => this.router.navigate(['articles']),
-      error: err => alert("An error occured")
+      error: () => this.signInError = true
     })
   }
 }
